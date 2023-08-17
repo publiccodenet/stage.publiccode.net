900d619974b2395d18329a705b884f40f4f3e34d
Max Carlson <>
2023-08-17 16:02:12 -0700

    Update
    + Fixes text around icons in 1a (set to fixed height instead of ‘hug’
    + Add icon and plausible.io analytics, move hotjar to head
    Built from AnimaPackage-Flex-m9Djx - synced as react, then downloaded as HTML

--- process.py
+++ process.py
@@ -18,21 +18,25 @@ patterns = [
     <meta name="viewport" content="width=1440, maximum-scale=1.0" />
     <link rel="shortcut icon" type="image/png" href="https://animaproject.s3.amazonaws.com/home/favicon.png" />""","""    <meta name="viewport" content="width=1440, maximum-scale=1.0" />"""),
 (r"""  </body>""","""     <script src="collapsible.js"></script>
-<script>
-    (function(h,o,t,j,a,r){
-        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:3580815,hjsv:6};
-        a=o.getElementsByTagName('head')[0];
-        r=o.createElement('script');r.async=1;
-        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-        a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-</script>
-</body>"""),
+  </body>"""),
 (r"""00u46-process-codes-landing-page.html""", """index.html"""),
 (r"""the agile development process. <br />You will continue running""", """the agile development process. <br /><br />You will continue running"""),
 (r"""expectations?<br />Throughout this process, you also have the crucial responsibility of ensuring that""", """expectations?<br /><br />Throughout this process, you also have the crucial responsibility of ensuring that"""),
 (r"""and expertise doesn’t disappear with individuals.<br />Your""", """and expertise doesn’t disappear with individuals.<br /><br />Your"""),
+(r"""  </head>""", """
+    <link rel="icon" href="https://brand.publiccode.net/logo/mark-128w128h.png">
+    <script>
+      (function(h,o,t,j,a,r){
+          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+          h._hjSettings={hjid:3580815,hjsv:6};
+          a=o.getElementsByTagName('head')[0];
+          r=o.createElement('script');r.async=1;
+          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+          a.appendChild(r);
+      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+    <script async defer data-domain="publiccode.net" src="https://plausible.io/js/plausible.js"></script>
+  </head>"""),
 ]
 
 for root, dirs, files in os.walk(directory):
