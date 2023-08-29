5bb0277bd0f455ed8136270fb1d5595c1c468178
Max Carlson <>
2023-08-29 10:42:18 -0600

    Update pagenames to match export

--- process.py
+++ process.py
@@ -4,19 +4,19 @@ import os
 import re
 
 # Rename index file
-old_filename = '00u46-process-codes-landing-page.html'
+old_filename = '00-process-codes-landing-page.html'
 new_filename = 'index.html'
 
 os.rename(old_filename, new_filename)
 
 # Rename breakpoint files
-old_filename = '1au46-getting-oriented-all-breakpoints.html'
-new_filename = '1au46-getting-oriented.html'
+old_filename = '1a-getting-oriented-all-breakpoints.html'
+new_filename = '1a-getting-oriented.html'
 
 os.rename(old_filename, new_filename)
 
-old_filename = '1bu46-capacity-building-u40mobileu41-all-breakpoints.html'
-new_filename = '1bu46-capacity-building.html'
+old_filename = '1b-capacity-building-u40mobileu41-all-breakpoints.html'
+new_filename = '1b-capacity-building.html'
 
 os.rename(old_filename, new_filename)
 
@@ -30,9 +30,9 @@ patterns = [
     <link rel="shortcut icon" type="image/png" href="https://animaproject.s3.amazonaws.com/home/favicon.png" />""","""    <meta name="viewport" content="width=1440, maximum-scale=1.0" />"""),
 (r"""  </body>""","""     <script src="collapsible.js"></script>
   </body>"""),
-(r"""00u46-process-codes-landing-page.html""", """index.html"""),
-(r"""1au46-getting-oriented-all-breakpoints.html""", """1au46-getting-oriented.html"""),
-(r"""1bu46-capacity-building-u40mobileu41-all-breakpoints.html""", """1bu46-capacity-building.html"""),
+(r"""00-process-codes-landing-page.html""", """index.html"""),
+(r"""1a-getting-oriented-all-breakpoints.html""", """1a-getting-oriented.html"""),
+(r"""1b-capacity-building-u40mobileu41-all-breakpoints.html""", """1b-capacity-building.html"""),
 (r"""the agile development process. <br />You will continue running""", """the agile development process. <br /><br />You will continue running"""),
 (r"""expectations?<br />Throughout this process, you also have the crucial responsibility of ensuring that""", """expectations?<br /><br />Throughout this process, you also have the crucial responsibility of ensuring that"""),
 (r"""and expertise doesn’t disappear with individuals.<br />Your""", """and expertise doesn’t disappear with individuals.<br /><br />Your"""),
@@ -66,9 +66,9 @@ for root, dirs, files in os.walk(directory):
 cssdirectory = './css/'
 csspatterns = [
 (r"""@import url\("https://px.animaapp.com/6406baa484a3afe9c63921de.6406baa605cc73851b593804.*.hcp.png"\);""", ''),
-(r"""00u46-process-codes-landing-page.html""", """index.html"""),
-(r"""1au46-getting-oriented-all-breakpoints.html""", """1au46-getting-oriented.html"""),
-(r"""1bu46-capacity-building-u40mobileu41-all-breakpoints.html""", """1bu46-capacity-building.html"""),
+(r"""00-process-codes-landing-page.html""", """index.html"""),
+(r"""1a-getting-oriented-all-breakpoints.html""", """1a-getting-oriented.html"""),
+(r"""1b-capacity-building-u40mobileu41-all-breakpoints.html""", """1b-capacity-building.html"""),
 ]
 # process css
 for root, dirs, files in os.walk(cssdirectory):
