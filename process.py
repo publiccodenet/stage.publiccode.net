ac246c6009db59c9d0ec70bdd7a0bffb8e1edc04
Max Carlson <>
2024-04-23 22:05:34 +0800

    Update based on AnimaPackage-Play-qGSwJ

--- process.py
+++ process.py
@@ -20,7 +20,7 @@ def updateFiles(patterns, path='.', ending='.html'):
 
 # files to move to match existing site structure
 files_to_move = [
-    (r'home-u40desktopu41-all-breakpoints.html', 'index.html'),
+    (r'product-model-u40desktopu41-ne.html', 'index.html'),
     # (r'codebase-stewardship.html', '/codebase-stewardship/index.html'),
     # (r'the-standard.html', '/standard-for-public-code/index.html'),
     # (r'codebases-we-work-with.html', '/codebases/index.html'),
